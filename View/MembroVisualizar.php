2ef44d16b804a8bf0836b732a962c8b77ce3662f
joaovitor32 <joaovitormunizlopes@gmail.com>
2019-06-19 16:29:14 -0300

    Visualizacao de membro

--- View/MembroVisualizar.php
+++ View/MembroVisualizar.php
@@ -6,8 +6,7 @@
     <head>
         <title>Visualizar</title>
         <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="../Css/MenuLateralSistema.css">
-        <link rel="stylesheet" type="text/css" href="Css/MembroVisualizar.css">
+        <link rel="stylesheet" type="text/css" href="./Css/BarraLateral/MenuLateralSistema.css">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -24,13 +23,17 @@
         </script>
     </head>
     <body>
+        <?php
+            include 'BarraLateralSistema.php';
+        ?>
         <section>
             <div class="container">
                 <div class="boxTabela">
-            
+                
                 </div>
             </div>
         </section>
+        <script type="text/javascript" src="Js/BarraLateral.js"></script>
         <script type="text/javascript" src="Js/MembroVisualizar.js"></script>
     </body>
 </html>
