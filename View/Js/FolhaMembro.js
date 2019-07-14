0c81e048686dc267f03e5cbbe3627a1ea430574f
joaovitor32 <joaovitormunizlopes@gmail.com>
2019-07-14 16:11:21 -0300

    Tabela projeto efeito no margin top

--- View/Js/FolhaMembro.js
+++ View/Js/FolhaMembro.js
@@ -29,6 +29,7 @@ function startFadeInDados(){
     },40);
     setInterval(function () {
         changeOpacityTabelaProjeto(tabelaProjeto);
+        changeMarginTopoTabelaProjeto(tabelaProjeto);
     },40);
 }
 function changeMarginTopoFoto(foto){
@@ -38,6 +39,7 @@ function changeMarginTopoFoto(foto){
         foto.style.marginTop=marginTop1+'%';
     }
 }
+//Foto
 function changeOpacityFoto(foto){
     var opacityFoto = Number(foto.style.getPropertyValue('opacity'));
     if(opacityFoto<1){
@@ -45,10 +47,25 @@ function changeOpacityFoto(foto){
         foto.style.opacity=opacityFoto;
     }
 }
+function changeMarginTopoFoto(foto){
+    var marginTop1=Number(foto.style.marginTop);
+    while(marginTop1<2){
+        marginTop1=marginTop1+0.025;
+        foto.style.marginTop=marginTop1+'%';
+    }
+}
+//Tabela projeto
 function changeOpacityTabelaProjeto(tabelaProjeto){
     var tabelaProjOpacity = Number(tabelaProjeto.style.getPropertyValue('opacity'));
     if(tabelaProjOpacity<1){
-        tabelaProjOpacity+=0.017;
+        tabelaProjOpacity+=0.04;
         tabelaProjeto.style.opacity=tabelaProjOpacity;
+    }
+}
+function changeMarginTopoTabelaProjeto(tabelaProjeto){
+    var tabelaProjetoTop1=Number(tabelaProjeto.style.marginTop);
+    while(tabelaProjetoTop1<2){
+        tabelaProjetoTop1=tabelaProjetoTop1+0.025;
+        tabelaProjeto.style.marginTop=tabelaProjetoTop1+'%';
     }
 }
